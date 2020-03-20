f28e3ba122045e6f2ab524ef83cf0b326859ddd2
marymargaryan <62055967+marymargaryan@users.noreply.github.com>
2020-03-20 13:59:20 +0400

    Update email_check.py

--- email_check.py
+++ email_check.py
@@ -1,18 +1,18 @@
-max_email_input_tries = 3
-tries = 0
+max_email_input_retries = 3
+retries = 0
 
 user_login_email = int(input("Please enter you email address"))
 email = 123
 
 
-while (not(user_login_email == email or tries == max_email_input_tries)):
-  if (tries < 3):
+while (not(user_login_email == email or retries >= max_email_input_retries)):
+  if (retries < 3):
     print ("The email address was incorrect")
-  user_login_email = int(input("Please enter you email address"))
-  tries = tries + 1
+  user_login_email = int(input("Please enter another email address"))
+  retries = retries + 1
 
 if (user_login_email == email):
-  print ("You have written the correct email")
+  print ("You have written an correct email")
 
 else:
-  print ("You have exceeded the amount of max tries")
+  print ("You have exceeded the amount of max retries")
