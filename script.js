363bf49f9564d0c7bed3af0054c011542e80f77c
annaasia <62056048+annaasia@users.noreply.github.com>
2020-05-01 13:50:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 function SubjectCharacters (inputtxt, minlength, maxlength) {
-var field = inputtxt.value;
-var mnlen = minlength;
-var mxlen = maxlength;
+  var field = inputtxt.value;
+  var mnlen = minlength;
+  var mxlen = maxlength;
 
-if(field.length <mnlen || field> mxlen.length)
-{ 
-alert("We suggest that you keep your subject between " +mnlen+ " and " +mxlen+ " characters");
-}
-else
-{ 
-alert('Your subject line looks great!');
-return true;
-}
+  if(field.length <mnlen || field> mxlen.length) { 
+    document.getElementById("subjectValid").innerHTML = "We suggest that you keep your subject between " +mnlen+ " and " +mxlen+ " characters";
+  } else { 
+    document.getElementById("subjectValid").innerHTML = "Looks good!";
+  }
 }
 
 function sendfunction() {
-  alert("Your email has been sent");
+  console.log("Your email has been sent");
 }
-
